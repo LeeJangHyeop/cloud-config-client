e456b1c5094527a8e9eecbc310773f7ea3166c92
firebat <powerfirebat@gmail.com>
2018-09-26 15:23:08 +0900

    commit

---
+++ src/main/java/com/mason/cloud/ConfigClientApplication.java
@@ -0,0 +1,27 @@
+package com.mason.cloud;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@SpringBootApplication
+public class ConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigClientApplication.class, args);
+	}
+
+	@Value("${mason.value}")
+	private String value;
+
+	@RestController
+	public class TestController {
+		@RequestMapping("/get")
+		public Mono<String> get() {
+			return Mono.just(value);
+		}
+	}
+}
