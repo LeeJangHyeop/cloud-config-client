4dc641a924a2254df8b9ab268ad5347ead8b6b5b
firebat <powerfirebat@gmail.com>
2018-09-26 15:57:13 +0900

    RefreshScope setting

--- src/main/java/com/mason/cloud/ConfigClientApplication.java
+++ src/main/java/com/mason/cloud/ConfigClientApplication.java
@@ -3,6 +3,7 @@ package com.mason.cloud;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
@@ -14,11 +15,12 @@ public class ConfigClientApplication {
 		SpringApplication.run(ConfigClientApplication.class, args);
 	}
 
-	@Value("${mason.value}")
-	private String value;
-
 	@RestController
+	@RefreshScope
 	public class TestController {
+        @Value("${mason.value}")
+        private String value;
+
 		@RequestMapping("/get")
 		public Mono<String> get() {
 			return Mono.just(value);
